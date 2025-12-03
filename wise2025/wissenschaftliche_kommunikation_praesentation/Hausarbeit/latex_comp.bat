bb6758a3f3d3799994a615559e84bc86642182c5
Luca Schmidt <76248448+LuMiSxh@users.noreply.github.com>
2025-12-03 10:29:48 +0100

    Revise thesis structure: update abstract, chapters, and add detailed methodology for comparative analysis of Tauri and Electron frameworks

---
+++ wise2025/wissenschaftliche_kommunikation_praesentation/Hausarbeit/latex_comp.bat
@@ -0,0 +1,25 @@
+@echo off
+REM Custom compilation script for TeXworks (PDFLaTeX + Biber)
+REM Usage: compile_biber.bat filename.tex
+
+REM Get the filename without extension
+for %%i in (%1) do set filename=%%~ni
+REM Get the directory path
+for %%i in (%1) do set dirname=%%~dpi
+
+REM Change to the directory of the tex file
+cd /d "%dirname%"
+
+echo [1/4] Running PDFLaTeX (Initial run)...
+pdflatex -synctex=1 -interaction=nonstopmode "%filename%"
+
+echo [2/4] Running Biber (Bibliography)...
+biber "%filename%"
+
+echo [3/4] Running PDFLaTeX (incorporating bibliography)...
+pdflatex -synctex=1 -interaction=nonstopmode "%filename%"
+
+echo [4/4] Running PDFLaTeX (resolving cross-references)...
+pdflatex -synctex=1 -interaction=nonstopmode "%filename%"
+
+echo Done.
